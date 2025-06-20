f9f5bb3bd7bf23fe8a389b1a93c267e5cc6b5794
abrar <abrarrayva.id@gmail.com>
2025-06-20 19:57:42 +0700

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 // Route Imports
-const authRoutes = require('.');
+// const authRoutes = require('./routes/authRoutes');
 
 // Settings
 app.set('view engine', 'ejs');
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/login', authRoutes);
+// app.use('/login', authRoutes);
 
 // Server
 const PORT = process.env.PORT || 3000;
