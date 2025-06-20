bf8809b9cc65a99c20b5b8835a80dd3d21b3d10a
abrar <abrarrayva.id@gmail.com>
2025-06-20 15:09:09 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const routes = require('./routes');
+
+// Settings
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', mainRoutes);
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
