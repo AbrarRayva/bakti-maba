8a0340175f64f8bec6e77efc40cc9520f9213b08
amanda-FA <amandabdlh@gmail.com>
2025-06-22 18:54:12 +0700

    perubahan
    perubahan alamat localhost dan logic yang diperlukan

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cookieParser = require('cookie-parser');
 
 // Route Imports
 const authRoutes = require("./routes/authRoutes");
+const leaderboardRouter = require('./routes/leaderboardRoutes');
+const forumRouter = require('./routes/forum');
+const chatbotRouter = require('./routes/chatbot');
 
 // Settings
 app.set("view engine", "ejs");
@@ -13,9 +17,13 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use("/login", authRoutes);
+app.use('/leaderboard', leaderboardRouter);
+app.use('/forum', forumRouter);
+app.use('/chatbot', chatbotRouter);
 
 // Default route redirect to login
 app.get("/", (req, res) => {
