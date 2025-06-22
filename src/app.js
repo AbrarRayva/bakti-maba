592282b23877bee8afdf2b8ad278ccc5957db273
abrar <abrarrayva.id@gmail.com>
2025-06-23 01:56:33 +0700

    Add Manajemen User

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override');
 
 // Route Imports
 const authRoutes = require("./routes/authRoutes");
@@ -31,6 +32,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 // Routes - Admin routes first to avoid conflicts
 app.use('/admin', adminRoutes);
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jadwalController = require('../controllers/jadwalController');
 const dashboardController = require('../controllers/dashboardController');
+const userController = require('../controllers/userController');
 
 // Admin dashboard route
 router.get('/dashboard', dashboardController.adminDashboard);
@@ -15,5 +16,16 @@ router.get('/jadwal/edit/:id', jadwalController.edit);
 router.post('/jadwal', jadwalController.store);
 router.put('/jadwal/:id', jadwalController.update);
 router.delete('/jadwal/:id', jadwalController.destroy);
+
+// Routes untuk admin users
+router.get('/users', userController.index);
+router.get('/users/create', userController.create);
+router.get('/users/edit/:id', userController.edit);
+
+// API routes untuk admin users
+router.post('/users', userController.store);
+router.put('/users/:id', userController.update);
+router.delete('/users/:id', userController.destroy);
+router.post('/users/:id/toggle-block', userController.toggleBlock);
 
 module.exports = router; 
