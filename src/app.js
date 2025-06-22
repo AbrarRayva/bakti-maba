a2844ae9b35c923ddca0c27ac413011c1845187c
abrar <abrarrayva.id@gmail.com>
2025-06-22 11:42:39 +0700

    Fix static

--- src/app.js
+++ src/app.js
@@ -10,10 +10,17 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 // Middleware
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
 app.use("/login", authRoutes);
+
+// Default route redirect to login
+app.get("/", (req, res) => {
+    res.redirect("/login");
+});
 
 // Server
 const PORT = process.env.PORT || 3000;
